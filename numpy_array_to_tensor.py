0f599efa85644e4e8faa8be468956c7c6c7569ed
pujan tamang <pujantamang92@gmail.com>
2023-09-03 18:03:51 +0100

    added tf.fill

--- numpy_array_to_tensor.py
+++ numpy_array_to_tensor.py
@@ -4,20 +4,30 @@ np_array = np.array([1, 2, 4])
 print(np_array)
 # [1 2 4]
 
-converted_tensor = tf.convert_to_tensor(np_array)
-print(converted_tensor)
+# converted_tensor = tf.convert_to_tensor(np_array)
+# print(converted_tensor)
 # tf.Tensor([1 2 4], shape=(3,), dtype=int32)
 
-eye_tensor = tf.eye(
-    num_rows=3,
-    num_columns=None,  # automatically equals to 3
-    batch_shape=None,  # specify how many iteration/subset you want
-    # batch leads the list if batch = (2, 4) then output dimension is (2, 4, 3, 3)
-    dtype=tf.dtypes.float32,  # data types
-    name=None
-)
-print(eye_tensor)
+# eye_tensor = tf.eye(
+#     num_rows=3,
+#     num_columns=None,  # automatically equals to 3
+#     batch_shape=None,  # specify how many iteration/subset you want
+#     # batch leads the list if batch = (2, 4) then output dimension is (2, 4, 3, 3)
+#     dtype=tf.dtypes.float32,  # data types
+#     name=None
+# )
+# print(eye_tensor)
 '''tf.Tensor(
 [[1. 0. 0.]
  [0. 1. 0.]
  [0. 0. 1.]], shape=(3, 3), dtype=float32)'''
+
+fill_tensor = tf.fill(
+    [2, 3], 9, name=None
+)
+print(fill_tensor)
+# it automatically creates tensor object of required dimensional of a certain value
+'''
+tf.Tensor(
+[[9 9 9]
+ [9 9 9]], shape=(2, 3), dtype=int32)'''
