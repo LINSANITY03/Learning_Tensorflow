3557c260ff61e79c1fc4baeb60b7d67b9db5a265
pujan tamang <pujantamang92@gmail.com>
2023-09-03 17:55:47 +0100

    creating identity matrix using tf.eye

--- numpy_array_to_tensor.py
+++ numpy_array_to_tensor.py
@@ -7,3 +7,16 @@ print(np_array)
 converted_tensor = tf.convert_to_tensor(np_array)
 print(converted_tensor)
 # tf.Tensor([1 2 4], shape=(3,), dtype=int32)
+
+eye_tensor = tf.eye(
+    num_rows=3,
+    num_columns=None,
+    batch_shape=None,
+    dtype=tf.dtypes.float32,
+    name=None
+)
+print(eye_tensor)
+'''tf.Tensor(
+[[1. 0. 0.]
+ [0. 1. 0.]
+ [0. 0. 1.]], shape=(3, 3), dtype=float32)'''
