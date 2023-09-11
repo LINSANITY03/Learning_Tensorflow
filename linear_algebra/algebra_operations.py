f977ddaefe4105a0d628f4e65a57f2ddc7cef6e3
pujan tamang <pujantamang92@gmail.com>
2023-09-11 12:30:14 +0100

    added inverse and determinant of a matrix

---
+++ linear_algebra/algebra_operations.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+
+# x_1 = tf.constant([[2, 4, 5], [56, 7, 24], [34, 76, 45]])
+# x_2 = tf.constant([[21, 64, 95], [52, 47, 64], [34, 76, 45]])
+# tensor_matmul = tf.linalg.matmul(
+#     x_1,
+#     x_2,
+#     transpose_a=False,
+#     transpose_b=False,
+#     adjoint_a=False,
+#     adjoint_b=False,
+#     a_is_sparse=False,
+#     b_is_sparse=False,
+#     output_type=None,
+#     name=None
+# )
+# print(tensor_matmul)
+# print(x_1@x_2)
+'''
+tf.Tensor(
+[[  420   696   671]
+ [ 2356  5737  6848]
+ [ 6196  9168 10119]], shape=(3, 3), dtype=int32)'''
+
+# tensor_band_part = tf.linalg.band_part(
+#     [[45, 1, 5], [7, 3, 9], [1, 3, 5]], 0, 1, name=None
+# )
+# each number indicate how many lines we want to show from centre as diagonal
+# 0 0 - diagonal part
+# 0 1 upper part
+# 1 0 lower part
+# print(tensor_band_part)
+det_input = tf.constant([[1.0, 5, 3], [4, 5, 6], [7, 8, 9]])
+# DT_HALF, DT_FLOAT, DT_DOUBLE, DT_COMPLEX64, DT_COMPLEX128
+# tensor_det = tf.linalg.det(
+#     det_input, name=None
+# )
+# print(tensor_det)  # tf.Tensor(17.999992, shape=(), dtype=float32)
+
+# tensor_inverse = tf.linalg.inv(
+#     det_input, adjoint=False, name=None
+# )
+# input should be a square matrix to be an inverse
+# print(tensor_inverse)
+'''
+tf.Tensor(
+[[-0.16666667 -1.1666673   0.83333373]
+ [ 0.3333333  -0.66666687  0.33333346]
+ [-0.16666666  1.5000006  -0.83333373]], shape=(3, 3), dtype=float32)'''
